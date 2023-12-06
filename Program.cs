1161115d206ae04cd2fc7f82d8c579df8549af4a
Çağatay Yıldız <yildiz.cagatay@hotmail.com>
2023-12-06 15:00:04 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿
+using PayCore.Client.models;
+using PayCore.Client.models.product;
+using RestSharp;
+
+
+HttpClient client = new HttpClient();
+client.BaseAddress = new Uri("https://localhost:44381");
+
+//var response = await client.GetAsync("/api/product");
+//var products = await response.Content.ReadAsAsync<List<GetAllProductsResponseDto>>();
+
+var response = await client.DeleteAsync("/api/product/1");
+
+
+
+
+
+//var product = new CreateProductRequestDto();
+//product.Name = "Huawei";
+//product.UnitPrice = 3000;
+
+//HttpResponseMessage response = await client.PostAsJsonAsync(
+//               "/api/product", product);
+
+
+
+
+
+
+
+//RestClient client = new RestClient("https://localhost:7185/api/");
+//RestRequest request = new RestRequest("Product", Method.Get);
+
+
+//var response = client.Execute<List<Product>>(request);
+
+//Console.WriteLine(response.StatusCode);
+
+
+//foreach (var item in response.Data)
+//{
+//    Console.WriteLine(item.Name);
+//}
+
+Console.ReadLine();
+
+
+
+
+
+
+
+
+
---
+++ models/product/CreateProductRequestDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayCore.Client.models.product
+{
+    public class CreateProductRequestDto
+    {
+        public string Name { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
